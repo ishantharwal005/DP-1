f3f68d2ad55e4cbc939d1518645143955ba513b6
Ishan Tharwal <ishan.tharwal@educationatwork.org>
2025-06-29 02:30:58 -0700

    Done CoinChange

---
+++ CoinChange.java
@@ -0,0 +1,24 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Had to revise basic syntax
+
+// ## Problem1 (https://leetcode.com/problems/coin-change/)
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You may assume that you have an infinite number of each kind of coin.
+// Example:
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+
+// Constraints:
+// 1 <= coins.length <= 12
+// 1 <= coins[i] <= 231 - 1
+// 0 <= amount <= 104
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+    }
+}
